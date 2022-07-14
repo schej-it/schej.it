65f793f2fe1533287f1049db9ad2459849f376d7
Jonathan Liu <liu.z.jonathan@gmail.com>
2022-07-14 02:24:55 -0700

    calendar scrolling

--- flutter_app/lib/components/calendar.dart
+++ flutter_app/lib/components/calendar.dart
@@ -1,6 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_app/constants/colors.dart';
 import 'package:flutter_app/constants/fonts.dart';
+import 'package:linked_scroll_controller/linked_scroll_controller.dart';
+import 'package:intl/intl.dart';
+import 'package:intl/date_symbol_data_local.dart';
+import 'package:sorted_list/sorted_list.dart';
+
+// Notes:
+// For calendar view, make the times column its own list view,
+// Make each day its own list view, and make the 3 day section a page view of list views
+// Overlaying the events look at this video: https://www.youtube.com/watch?v=OOEyJ0ct0Sg
 
 class Calendar extends StatefulWidget {
   const Calendar({Key? key}) : super(key: key);
@@ -10,44 +19,40 @@ class Calendar extends StatefulWidget {
 }
 
 class _CalendarState extends State<Calendar> {
-  final double timeColWidth = 50;
-  final double timeRowHeight = 45;
+  // Constants
+  final double _timeColWidth = 50;
+  final double _timeRowHeight = 45;
+
+  // Controllers
+  late final PageController _pageController;
+  late final LinkedScrollControllerGroup _controllers;
+  late final ScrollController _timeScrollController;
+  late final Map<DateTime, ScrollController> _dayScrollControllers = <DateTime, ScrollController>{};
+
+  // Other variables
+  final DateTime _curDate = DateTime.now();
+  final SortedList<DateTime> _loadedDates = SortedList<DateTime>((a, b) => a.compareTo(b));
+  final _timeStrings = <String>[];
 
   @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        _buildDaySection(),
-        const Divider(color: SchejColors.darkGray),
-        Expanded(child: _buildCalendarSection()),
-      ],
-    );
-  }
+  void initState() {
+    super.initState();
+    initializeDateFormatting();
 
-  Widget _buildDaySection() {
-    return Row(
-      // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      children: [
-        SizedBox(width: timeColWidth),
-        Expanded(child: _buildDay('Tue', 5)),
-        Expanded(child: _buildDay('Wed', 6)),
-        Expanded(child: _buildDay('Thu', 7)),
-      ],
-    );
-  }
+    _controllers = LinkedScrollControllerGroup();
+    _timeScrollController = _controllers.addAndGet();
+    for (int i = -3; i <= 3; ++i) {
+      final date = _curDate.add(Duration(days: i));
+      _loadedDates.add(date);
+      _dayScrollControllers[date] = _controllers.addAndGet();
+    }
+    // _controllers.jumpTo(8.25 * _timeRowHeight);
 
-  Widget _buildDay(String dayText, int dateNum) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: [
-        Text(dayText, style: SchejFonts.body),
-        Text(dateNum.toString(), style: SchejFonts.header),
-      ],
+    _pageController = PageController(
+      viewportFraction: 1 / 3, 
+      initialPage: _loadedDates.indexOf(_curDate),
     );
-  }
 
-  Widget _buildCalendarSection() {
-    final timeStrings = <String>[];
     for (int i = 1; i < 24; ++i) {
       String timeText;
       if (i < 12) {
@@ -57,49 +62,147 @@ class _CalendarState extends State<Calendar> {
       } else {
         timeText = '${i - 12} PM';
       }
-      timeStrings.add(timeText);
+      _timeStrings.add(timeText);
+    }
+  }
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    _timeScrollController.dispose();
+    for (var controller in _dayScrollControllers.values) {
+      controller.dispose();
+    }
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        _buildTimeColumn(),
+        Expanded(child: _buildDaySection()),
+      ],
+    );
+    // return Column(
+    //   children: [
+    //     _buildDaySection(),
+    //     const Divider(color: SchejColors.darkGray),
+    //     Expanded(child: _buildCalendarSection()),
+    //   ],
+    // );
+  }
+
+  Widget _buildDaySection() {
+    final days = <Widget>[];
+    for (final date in _loadedDates) {
+      days.add(
+        _buildDay(date)
+      );
     }
 
-    return ListView.builder(
-      scrollDirection: Axis.vertical,
-      itemCount: timeStrings.length,
-      controller: ScrollController(initialScrollOffset: 8.25*timeRowHeight),
-      itemBuilder: (BuildContext context, int index) {
-        return SizedBox(
-          height: timeRowHeight,
-          child: Row(
+    return FractionallySizedBox(
+      heightFactor: 1,
+      child: PageView(
+        controller: _pageController,
+        children: days,
+      ),
+    );
+  }
+
+  Widget _buildDay(DateTime date) {
+    String dayText = DateFormat.E().format(date);
+    int dateNum = date.day;
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        SizedBox(
+          height: 50,
+          child: Column(
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              SizedBox(
-                width: timeColWidth,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.only(right: 8.0),
-                      child: Align(
-                        alignment: Alignment.centerRight,
-                        child: Text(
-                          timeStrings[index],
-                          style: SchejFonts.body.copyWith(color: SchejColors.darkGray),
-                          textAlign: TextAlign.right,
-                        ),
-                      ),
-                    ),
-                    const SizedBox(width: 5, child: Divider(color: SchejColors.lightGray, thickness: 1.15)),
-                  ],
-                ),
-              ),
-              const VerticalDivider(width: 1.15, thickness: 1.15, color: SchejColors.lightGray),
-              const Expanded(child: Divider(color: SchejColors.lightGray, thickness: 1.15)),
-              const VerticalDivider(width: 1.15, thickness: 1.15, color: SchejColors.lightGray),
-              const Expanded(child: Divider(color: SchejColors.lightGray, thickness: 1.15)),
-              const VerticalDivider(width: 1.15, thickness: 1.15, color: SchejColors.lightGray),
-              const Expanded(child: Divider(color: SchejColors.lightGray, thickness: 1.15)),
+              Text(dayText, style: SchejFonts.body),
+              Text(dateNum.toString(), style: SchejFonts.header),
             ],
           ),
-        );
-      },
+        ),
+        const Divider(color: SchejColors.darkGray),
+        Expanded(
+          child: ListView.builder(
+            scrollDirection: Axis.vertical,
+            itemCount: _timeStrings.length,
+            controller: _dayScrollControllers[date],
+            itemBuilder: (BuildContext context, int index) {
+              return SizedBox(
+                height: _timeRowHeight,
+                child: Row(
+                  children: const [
+                    VerticalDivider(
+                      width: 1.15,
+                      thickness: 1.15,
+                      color: SchejColors.lightGray,
+                    ),
+                    Expanded(
+                      child: Divider(
+                        thickness: 1.15,
+                        color: SchejColors.lightGray,
+                      ),
+                    ),
+                  ],
+                ),
+              );
+            },
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildTimeColumn() {
+    return SizedBox(
+      width: _timeColWidth,
+      child: Column(
+        children: [
+          const SizedBox(height: 50),
+          const Divider(color: SchejColors.darkGray),
+          Expanded(
+            child: ListView.builder(
+              scrollDirection: Axis.vertical,
+              itemCount: _timeStrings.length,
+              controller: _timeScrollController,
+              itemBuilder: (BuildContext context, int index) {
+                return SizedBox(
+                  height: _timeRowHeight,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(right: 8.0),
+                        child: Align(
+                          alignment: Alignment.centerRight,
+                          child: Text(
+                            _timeStrings[index],
+                            style:
+                                SchejFonts.body.copyWith(color: SchejColors.darkGray),
+                            textAlign: TextAlign.right,
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        width: 5,
+                        child: Divider(
+                          color: SchejColors.lightGray,
+                          thickness: 1.15,
+                        ),
+                      ),
+                    ],
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
