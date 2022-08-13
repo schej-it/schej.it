aec381585796e936df7eedf3fd210fafc263610b
Jonathan Liu <liu.z.jonathan@gmail.com>
2022-08-12 22:11:06 -0700

    sign in page frontend

--- flutter_app/lib/pages/sign_in.dart
+++ flutter_app/lib/pages/sign_in.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app/constants/colors.dart';
+import 'package:flutter_app/constants/constants.dart';
+import 'package:flutter_app/constants/fonts.dart';
 import 'package:flutter_app/models/auth_service.dart';
+import 'package:flutter_signin_button/button_list.dart';
+import 'package:flutter_signin_button/button_view.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:provider/provider.dart';
 
 class SignInPage extends StatefulWidget {
@@ -19,9 +25,42 @@ class _SignInPageState extends State<SignInPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: ElevatedButton(
-          onPressed: _signIn,
-          child: const Text('Sign in'),
+        child: Column(
+          children: [
+            Expanded(
+              child: SvgPicture.asset(
+                'assets/img/schej_logo_big.svg',
+                semanticsLabel: 'Schej logo',
+                width: 150,
+              ),
+            ),
+            Expanded(
+              child: CustomPaint(
+                painter: _CirclePainter(),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(bottom: 50),
+                      child: Text(
+                        'Scheduling made simple',
+                        style: SchejFonts.header
+                            .copyWith(color: SchejColors.white),
+                      ),
+                    ),
+                    SignInButton(
+                      Buttons.Google,
+                      onPressed: _signIn,
+                      text: 'Continue with Google',
+                      shape: RoundedRectangleBorder(
+                        borderRadius: SchejConstants.borderRadius,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
@@ -33,5 +72,22 @@ class _SignInPageState extends State<SignInPage> {
     if (success) {
       widget.onSignIn();
     } else {}
+  }
+}
+
+class _CirclePainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paint = Paint()
+      ..color = SchejColors.green
+      ..style = PaintingStyle.fill;
+
+    canvas.drawCircle(
+        Offset(0.5 * size.width, .8 * size.height), 1.3 * size.width, paint);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return false;
   }
 }
