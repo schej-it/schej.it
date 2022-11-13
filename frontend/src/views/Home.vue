62bc5c0914f26c8a9a5b5608ef202f1e6b07af21
Tony Xin <32372239+TtonyxX@users.noreply.github.com>
2022-11-12 16:08:52 -0800

    Reversed events order to be newest first

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -19,7 +19,7 @@
         <div v-else class="tw-grid tw-grid-cols-1 sm:tw-grid-cols-2 md:tw-grid-cols-3 tw-gap-2 tw-my-3">
           <EventItem  
             class="tw-cursor-pointer"
-            v-for="event, i in eventType.events" 
+            v-for="event, i in eventType.events.reverse()" 
             :key="i"
             :event="event" 
             @click="goToEvent(event._id)"
