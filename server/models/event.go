d34fcded3369a78324d16dc988f639f530836f75
Jonathan Liu <liu.z.jonathan@gmail.com>
2022-12-29 01:16:40 -0800

    change date format to string array

--- server/models/event.go
+++ server/models/event.go
@@ -4,15 +4,19 @@ import "go.mongodb.org/mongo-driver/bson/primitive"
 
 // Representation of an Event in the mongoDB database
 type Event struct {
-	Id        primitive.ObjectID   `json:"_id" bson:"_id,omitempty"`
-	OwnerId   primitive.ObjectID   `json:"ownerId" bson:"ownerId,omitempty"`
-	Name      string               `json:"name" bson:"name,omitempty"`
-	StartDate primitive.DateTime   `json:"startDate" bson:"startDate,omitempty"`
-	EndDate   primitive.DateTime   `json:"endDate" bson:"endDate,omitempty"`
-	StartTime int                  `json:"startTime" bson:"startTime,omitempty"`
-	EndTime   int                  `json:"endTime" bson:"endTime,omitempty"`
-	Dates     []primitive.DateTime `json:"dates" bson:"dates,omitempty"`
-	Responses map[string]Response  `json:"responses" bson:"responses"`
+	Id        primitive.ObjectID `json:"_id" bson:"_id,omitempty"`
+	OwnerId   primitive.ObjectID `json:"ownerId" bson:"ownerId,omitempty"`
+	Name      string             `json:"name" bson:"name,omitempty"`
+	StartDate primitive.DateTime `json:"startDate" bson:"startDate,omitempty"`
+	EndDate   primitive.DateTime `json:"endDate" bson:"endDate,omitempty"`
+
+	// StartTime and EndTime are UTC hours, dates are an array of utc dates
+	StartTime int      `json:"startTime" bson:"startTime,omitempty"`
+	EndTime   int      `json:"endTime" bson:"endTime,omitempty"`
+	Dates     []string `json:"dates" bson:"dates,omitempty"`
+
+	// Availability responses
+	Responses map[string]Response `json:"responses" bson:"responses"`
 }
 
 // A response object containing an array of times that the given user is available
