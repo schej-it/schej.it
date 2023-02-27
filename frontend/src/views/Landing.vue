87a7b1d1adb07dc66f6fced68902d440af6c5c37
Tony Xin <32372239+TtonyxX@users.noreply.github.com>
2023-02-26 21:54:29 -0800

    What if schej section

--- frontend/src/views/Landing.vue
+++ frontend/src/views/Landing.vue
@@ -1,37 +1,37 @@
 <template>
   <div>
-    <v-dialog 
-      v-model="dialog"
-      :width="400"
-    >
+    <v-dialog v-model="dialog" :width="400">
       <v-card>
         <v-card-title>
           <span v-if="dialogType === DIALOG_TYPES.SIGN_IN">Sign in</span>
           <span v-else-if="dialogType === DIALOG_TYPES.SIGN_UP">Sign up</span>
         </v-card-title>
         <v-card-text class="tw-flex tw-flex-col tw-items-center">
-          <SignInGoogleBtn
-            @click="signInGoogle"
-            dark
-          />
-          <div class="tw-text-xs tw-text-center">By continuing, you agree to our <router-link class="tw-text-blue" :to="{ name: 'privacy-policy' }">privacy policy</router-link></div>
+          <SignInGoogleBtn @click="signInGoogle" dark />
+          <div class="tw-text-xs tw-text-center">
+            By continuing, you agree to our
+            <router-link class="tw-text-blue" :to="{ name: 'privacy-policy' }"
+              >privacy policy</router-link
+            >
+          </div>
         </v-card-text>
       </v-card>
     </v-dialog>
 
     <div class="tw-h-screen tw-flex tw-flex-col">
-
       <!-- Header -->
       <div class="tw-mb-12">
-        <div class="tw-pt-5 tw-px-5 tw-flex tw-justify-between tw-items-center tw-max-w-6xl tw-m-auto">
-            <v-img
-              alt="schej Logo"
-              class="shrink tw-cursor-pointer"
-              contain
-              src="@/assets/schej_logo_with_text.png"
-              transition="scale-transition"
-              :width="logoWidth"
-            />
+        <div
+          class="tw-pt-5 tw-px-5 tw-flex tw-justify-between tw-items-center tw-max-w-6xl tw-m-auto"
+        >
+          <v-img
+            alt="schej Logo"
+            class="shrink tw-cursor-pointer"
+            contain
+            src="@/assets/schej_logo_with_text.png"
+            transition="scale-transition"
+            :width="logoWidth"
+          />
 
           <v-btn text @click="signIn">Sign in</v-btn>
         </div>
@@ -39,49 +39,91 @@
           <div
             class="tw-mt-16 tw-mb-4 lg:tw-mb-10 tw-text-2xl sm:tw-text-5xl lg:tw-text-6xl tw-font-medium tw-text-center"
           >
-            Finding a time to meet,<br> made simple.
+            Finding a time to meet,<br />
+            made simple.
           </div>
           <div
             class="tw-mb-4 lg:tw-mb-10 sm:tw-text-xl lg:tw-text-2xl tw-text-center"
           >
-            It's like when2meet with <br v-if="isPhone"> Google Calendar integration
+            It's like when2meet with <br v-if="isPhone" />
+            Google Calendar integration
           </div>
-          <v-btn class="tw-bg-green tw-rounded-lg" dark @click="getStarted" :large="$vuetify.breakpoint.smAndUp" :x-large="$vuetify.breakpoint.mdAndUp"
+          <v-btn
+            class="tw-bg-green tw-rounded-lg"
+            dark
+            @click="getStarted"
+            :large="$vuetify.breakpoint.smAndUp"
+            :x-large="$vuetify.breakpoint.mdAndUp"
             >Let's schej it</v-btn
           >
         </div>
       </div>
 
       <!-- Calendar -->
-      <div class="tw-flex-1 md:tw-flex md:tw-justify-center tw-relative tw-pb-12">
-        <div 
+      <div
+        class="tw-flex-1 md:tw-flex md:tw-justify-center tw-relative tw-pb-12"
+      >
+        <div
           class="tw-absolute tw-bg-green tw-w-full"
-          style="top: 10rem; height: calc(100% - 10rem);"
-        >
-        </div>
+          style="top: 10rem; height: calc(100% - 10rem)"
+        ></div>
         <div>
           <LandingPageCalendar />
         </div>
       </div>
 
-      <!-- Privacy Policy -->
-      <div class="tw-bg-green tw-flex tw-flex-col">
-        <div class="tw-text-center tw-text-white tw-m-2">	
-          <router-link class="tw-text-white tw-font-medium" :to="{ path: 'privacy-policy' }">Privacy Policy</router-link>
+      <!-- What is Schej? -->
+      <div class="tw-grid sm:tw-grid-cols-1 md:tw-grid-cols-2 tw-bg-white tw-p-8 sm:tw-p-28">
+        <div class="tw-flex tw-flex-col tw-pr-10 lg:tw-pt-5">
+          <div class="tw-text-2xl sm:tw-text-5xl lg:tw-text-6xl tw-font-medium">What is schej?</div>
+          <div class="tw-mt-8 tw-grid tw-grid-cols-1 tw-gap-4 sm:tw-text-xl lg:tw-text-2xl">
+            <div>
+              schej is a group scheduling platform that helps you find a time to
+              meet.
+            </div>
+            <div>
+              Users fill out their availability with the help of Google
+              Calendar, and a heat map is generated showing when everybody is
+              available.
+            </div>
+            <div>
+              It's <span class="tw-font-bold">completely free</span> to use, and it looks great on mobile.
+            </div>
+          </div>
+        </div>
+        <div class="tw-h-96 tw-mt-5 md:tw-mt-0">
+          <iframe
+            class="tw-w-full tw-h-full"
+            src="https://www.youtube.com/embed/Wzth9Ov7bkI?fs=0&color=white&rel=0&controls=0"
+            title="Demo of Schej"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowfullscreen
+          ></iframe>
         </div>
       </div>
 
+      <!-- Privacy Policy -->
+      <div class="tw-bg-green tw-flex tw-flex-col">
+        <div class="tw-text-center tw-text-white tw-m-2">
+          <router-link
+            class="tw-text-white tw-font-medium"
+            :to="{ path: 'privacy-policy' }"
+            >Privacy Policy</router-link
+          >
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-import LandingPageCalendar from '@/components/LandingPageCalendar.vue'
-import { isPhone, signInGoogle } from '@/utils'
-import SignInGoogleBtn from '@/components/SignInGoogleBtn.vue'
+import LandingPageCalendar from "@/components/LandingPageCalendar.vue"
+import { isPhone, signInGoogle } from "@/utils"
+import SignInGoogleBtn from "@/components/SignInGoogleBtn.vue"
 
 export default {
-  name: 'Landing',
+  name: "Landing",
 
   components: {
     LandingPageCalendar,
