f64d966554e43e23ca6c2dbc9f05ae8a3481c42f
Jonathan Liu <liu.z.jonathan@gmail.com>
2023-03-15 12:35:49 -0700

    use mailjet instead of mailchimp

--- server/utils/utils.go
+++ server/utils/utils.go
@@ -146,6 +146,7 @@ func GetCalendarEvents(accessToken string, calendarId string, timeMin time.Time,
 
 	// Parse the response
 	var res Response
+	// defer resp.Body.close() ??
 	if err := json.NewDecoder(resp.Body).Decode(&res); err != nil {
 		logger.StdErr.Panicln(err)
 	}
@@ -235,6 +236,100 @@ func AddUserToMailchimp(email string, firstName string, lastName string) {
 	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
 		logger.StdErr.Println(err)
+	}
+	defer resp.Body.Close()
+}
+
+func AddUserToMailjet(email string, firstName string, lastName string, picture string) {
+	// Adds the given user to the Mailjet contact list
+	apiKey := os.Getenv("MAILJET_API_KEY")
+	apiSecret := os.Getenv("MAILJET_API_SECRET")
+	listId := os.Getenv("MAILJET_LIST_ID")
+
+	// Create contact
+	// POST https://api.mailjet.com/v3/REST/contact {"EMAIL", email}
+	// contactId = result.Data[0].ID (integer)
+	body, _ := json.Marshal(bson.M{
+		"Email": email,
+	})
+	bodyBuffer := bytes.NewBuffer(body)
+
+	req, _ := http.NewRequest("POST", "https://api.mailjet.com/v3/REST/contact", bodyBuffer)
+	req.SetBasicAuth(apiKey, apiSecret)
+	req.Header.Set("Content-Type", "application/json")
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		logger.StdErr.Println(err)
+		return
+	}
+	defer resp.Body.Close()
+
+	result := struct {
+		Data []struct {
+			ID int
+		}
+	}{}
+	if err := json.NewDecoder(resp.Body).Decode(&result); err != nil {
+		logger.StdErr.Println(err)
+		return
+	}
+
+	contactId := result.Data[0].ID
+
+	// Update contact metadata
+	// PUT https://api.mailjet.com/v3/REST/contactdata/$contactId
+	// { "Data": [{"Name": "firstname", "Value":"first name!"}] }
+	body, _ = json.Marshal(bson.M{
+		"Data": bson.A{
+			bson.M{
+				"Name":  "firstname",
+				"Value": firstName,
+			},
+			bson.M{
+				"Name":  "lastname",
+				"Value": lastName,
+			},
+			bson.M{
+				"Name":  "picture",
+				"Value": picture,
+			},
+		},
+	})
+	bodyBuffer = bytes.NewBuffer(body)
+
+	req, _ = http.NewRequest("PUT", fmt.Sprintf("https://api.mailjet.com/v3/REST/contactdata/%d", contactId), bodyBuffer)
+	req.SetBasicAuth(apiKey, apiSecret)
+	req.Header.Set("Content-Type", "application/json")
+
+	resp, err = http.DefaultClient.Do(req)
+	if err != nil {
+		logger.StdErr.Println(err)
+		return
+	}
+	defer resp.Body.Close()
+
+	// Add contact to "schej users" contact list
+	// POST https://api.mailjet.com/v3/REST/contact/$contactId/managecontactslists
+	// '{ "ContactsLists": [{"Action": "addforce", "ListID": "10219365"}] }'
+	body, _ = json.Marshal(bson.M{
+		"ContactsLists": bson.A{
+			bson.M{
+				"Action": "addforce",
+				"ListID": listId,
+			},
+		},
+	})
+	bodyBuffer = bytes.NewBuffer(body)
+
+	req, _ = http.NewRequest("POST", fmt.Sprintf("https://api.mailjet.com/v3/REST/contact/%d/managecontactslists", contactId), bodyBuffer)
+	req.SetBasicAuth(apiKey, apiSecret)
+	req.Header.Set("Content-Type", "application/json")
+
+	resp, err = http.DefaultClient.Do(req)
+	if err != nil {
+		logger.StdErr.Println(err)
+		return
 	}
 	defer resp.Body.Close()
 }
