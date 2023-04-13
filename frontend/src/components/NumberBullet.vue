8acc4e7bfe5513be37a20e65975612edc9e3febc
Jonathan Liu <liu.z.jonathan@gmail.com>
2023-04-12 20:30:58 -0700

    basic layout

---
+++ frontend/src/components/NumberBullet.vue
@@ -0,0 +1,22 @@
+<!-- Displays a number within a green circle -->
+<template>
+  <div class="
+    tw-grid tw-place-content-center 
+    tw-rounded-full 
+    tw-shrink-0 
+    tw-bg-green tw-text-white 
+    md:tw-w-10 md:tw-h-10 
+    tw-w-7 tw-h-7 
+    md:tw-text-xl 
+    tw-text-base
+    tw-font-medium
+  ">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NumberBullet',
+}
+</script>
--- frontend/src/views/Landing.vue
+++ frontend/src/views/Landing.vue
@@ -18,7 +18,7 @@
       </v-card>
     </v-dialog>
 
-    <div class="tw-h-screen tw-flex tw-flex-col">
+    <div class="tw-min-h-screen tw-flex tw-flex-col">
       <!-- Header -->
       <div class="tw-mb-12">
         <div
@@ -35,18 +35,14 @@
 
           <v-btn text @click="signIn">Sign in</v-btn>
         </div>
+
+        <!-- Hero -->
         <div class="tw-flex tw-flex-col tw-items-center">
           <div
             class="tw-mt-16 tw-mb-4 lg:tw-mb-10 tw-text-2xl sm:tw-text-5xl lg:tw-text-6xl tw-font-medium tw-text-center"
           >
             Finding a time to meet,<br />
             made simple.
-          </div>
-          <div
-            class="tw-mb-4 lg:tw-mb-10 sm:tw-text-xl lg:tw-text-2xl tw-text-center"
-          >
-            It's like when2meet with <br v-if="isPhone" />
-            Google Calendar integration
           </div>
           <v-btn
             id="lets-schej-it-btn"
@@ -57,13 +53,13 @@
             :x-large="$vuetify.breakpoint.mdAndUp"
             >Let's schej it</v-btn
           >
+          <a href="#how-it-works" class="tw-text-blue tw-mt-4 tw-text-sm"
+            >How it works</a>
         </div>
       </div>
 
       <!-- Calendar -->
-      <div
-        class="tw-flex-1 md:tw-flex md:tw-justify-center tw-relative tw-pb-12 tw-mb-12 md:tw-mb-24"
-      >
+      <div class="tw-flex-1 md:tw-flex md:tw-justify-center tw-relative tw-pb-12">
         <div
           class="tw-absolute tw-bg-green tw-w-full"
           style="top: 10rem; height: calc(100% - 10rem)"
@@ -72,82 +68,84 @@
           <LandingPageCalendar />
         </div>
       </div>
+    </div>
 
-      <!-- What is schej? -->
+    <!-- How it works -->
+    <div id="how-it-works" class="tw-pt-12 tw-grid tw-place-content-center tw-px-4">
+      <div class="tw-flex tw-flex-col tw-gap-4 tw-mx-auto">
+        <Header class="tw-text-center">How it works</Header>
+        <div v-for="step, i in howItWorksSteps" :key="i" class="tw-flex tw-items-center tw-gap-2">
+          <NumberBullet>{{ i+1 }}</NumberBullet>
+          <div class="md:tw-text-xl tw-text-base tw-font-medium">{{ step }}</div>
+        </div>
+      </div>
+      <div class="
+        tw-mt-10 tw-mb-6 md:tw-mt-20 md:tw-mb-12 
+        tw-text-3xl md:tw-text-6xl  
+        tw-font-medium tw-text-center
+      ">It's that simple.</div>
+      <v-img 
+        alt="schej character"
+        src="@/assets/schej_character.png"
+        :max-height="isPhone ? 200 : 300"
+        transition="scale-transition"
+        contain
+        class="-tw-mb-12"
+      />
+    </div>
+
+    <!-- Video -->
+    <div class="
+      tw-flex tw-bg-green 
+      tw-px-4 
+      tw-pt-20 
+      tw-pb-4 md:tw-pb-12
+      tw-justify-center
+    ">
+      <div class="md:tw-h-96 sm:tw-h-80 tw-h-64 tw-max-w-3xl tw-flex-1">
+        <iframe
+          class="tw-w-full tw-h-full"
+          src="https://www.youtube.com/embed/Wzth9Ov7bkI?fs=0&color=white&rel=0&controls=0"
+          title="Demo of Schej"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowfullscreen
+        ></iframe>
+      </div>
+    </div>
+
+    <!-- FAQ -->
+    <div class="tw-flex tw-justify-center tw-pt-12">
       <div
-        id="how-it-works"
-        class="lg:tw-flex lg:tw-items-center tw-mx-4 md:tw-mx-8 xl:tw-mx-auto tw-max-w-7xl tw-mb-12 md:tw-mb-24"
+        class="tw-flex-1 tw-mx-4 sm:tw-mx-16 tw-mb-12 tw-max-w-3xl"
       >
-        <!-- Text -->
-        <div class="tw-flex-1 tw-flex tw-flex-col tw-pr-10 lg:tw-pt-3">
+        <div id="faq-section" class="lg:tw-pt-3 tw-text-center">
           <Header>
-            What is schej?
+            Frequently Asked Questions
           </Header>
           <div
-            class="tw-grid tw-grid-cols-1 tw-gap-6 sm:tw-text-lg lg:tw-text-xl"
+            class="tw-grid tw-grid-cols-1 tw-gap-3 sm:tw-text-xl lg:tw-text-2xl"
           >
-            <div>
-              schej is a group scheduling platform that helps you find a time to
-              meet.
-            </div>
-            <div>
-              Users fill out their availability with the help of Google
-              Calendar, and a heat map is generated showing when everybody is
-              available.
-            </div>
-            <div>
-              It's <span class="tw-font-bold">completely free</span> to use, and
-              it looks great on mobile.
-            </div>
-          </div>
-        </div>
-
-        <!-- Video -->
-        <div class="tw-flex-1 tw-h-96 tw-mt-5">
-          <iframe
-            class="tw-w-full tw-h-full"
-            src="https://www.youtube.com/embed/Wzth9Ov7bkI?fs=0&color=white&rel=0&controls=0"
-            title="Demo of Schej"
-            frameborder="0"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-            allowfullscreen
-          ></iframe>
-        </div>
-      </div>
-
-      <!-- FAQ -->
-      <div class="tw-flex tw-justify-center">
-        <div
-          class="tw-flex-1 tw-mx-4 sm:tw-mx-16 tw-mb-12 tw-max-w-3xl"
-        >
-          <div id="faq-section" class="lg:tw-pt-3 tw-text-center">
-            <Header>
-              Frequently Asked Questions
-            </Header>
-            <div
-              class="tw-grid tw-grid-cols-1 tw-gap-3 sm:tw-text-xl lg:tw-text-2xl"
-            >
-              <FAQ 
-                v-for="faq in faqs"
-                :key="faq.question"
-                v-bind="faq"
-              />
-            </div>
+            <FAQ 
+              v-for="faq in faqs"
+              :key="faq.question"
+              v-bind="faq"
+            />
           </div>
         </div>
       </div>
+    </div>
 
-      <!-- Privacy Policy -->
-      <div class="tw-bg-green tw-flex tw-flex-col">
-        <div
-          class="tw-flex tw-flex-row tw-justify-around tw-m-2 tw-py-4 tw-font-medium"
+    <!-- Privacy Policy -->
+    <div class="tw-bg-green tw-flex tw-flex-col">
+      <div
+        class="tw-flex tw-flex-row tw-justify-around tw-m-2 tw-py-4 tw-font-medium"
+      >
+        <a class="tw-text-white" href="#how-it-works" >How it works</a>
+        <router-link class="tw-text-white" :to="{ path: 'privacy-policy' }"
+          >Privacy Policy</router-link
         >
-          <a class="tw-text-white" href="#how-it-works" >How it works</a>
-          <router-link class="tw-text-white" :to="{ path: 'privacy-policy' }"
-            >Privacy Policy</router-link
-          >
-          <a class="tw-text-white" @click="signIn">Sign in</a>
-        </div>
+        <a class="tw-text-white" @click="signIn">Sign in</a>
       </div>
     </div>
   </div>
@@ -159,6 +157,7 @@ import { isPhone, signInGoogle } from "@/utils"
 import SignInGoogleBtn from "@/components/SignInGoogleBtn.vue"
 import FAQ from "@/components/FAQ.vue"
 import Header from "@/components/Header.vue"
+import NumberBullet from "@/components/NumberBullet.vue"
 
 export default {
   name: "Landing",
@@ -168,7 +167,8 @@ export default {
     SignInGoogleBtn,
     FAQ,
     Header,
-  },
+    NumberBullet
+},
 
   data: () => ({
     dialog: false,
@@ -177,6 +177,11 @@ export default {
       SIGN_IN: 0,
       SIGN_UP: 1,
     },
+    howItWorksSteps: [
+      'Create a schej',
+      'Automatically fill out your availability with Google Calendar',
+      'Share the schej and find the best time to meet!',
+    ],
     faqs: [
       {
         question: 'Do I need to sign in with Google in order to use schej?',
