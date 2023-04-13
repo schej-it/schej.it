f91ff818f4069600ad284dfb2df1d9c779187bd3
Jonathan Liu <liu.z.jonathan@gmail.com>
2023-04-12 20:53:55 -0700

    format landing.vue with prettier

--- frontend/src/views/Landing.vue
+++ frontend/src/views/Landing.vue
@@ -54,12 +54,15 @@
             >Let's schej it</v-btn
           >
           <a href="#how-it-works" class="tw-text-blue tw-mt-4 tw-text-sm"
-            >How it works</a>
+            >How it works</a
+          >
         </div>
       </div>
 
       <!-- Calendar -->
-      <div class="tw-flex-1 md:tw-flex md:tw-justify-center tw-relative tw-pb-12">
+      <div
+        class="tw-flex-1 md:tw-flex md:tw-justify-center tw-relative tw-pb-12"
+      >
         <div
           class="tw-absolute tw-bg-green tw-w-full"
           style="top: 10rem; height: calc(100% - 10rem)"
@@ -71,20 +74,30 @@
     </div>
 
     <!-- How it works -->
-    <div id="how-it-works" class="tw-pt-12 tw-grid tw-place-content-center tw-px-4">
+    <div
+      id="how-it-works"
+      class="tw-pt-12 tw-grid tw-place-content-center tw-px-4"
+    >
       <div class="tw-flex tw-flex-col tw-gap-4 tw-mx-auto">
         <Header class="tw-text-center">How it works</Header>
-        <div v-for="step, i in howItWorksSteps" :key="i" class="tw-flex tw-items-center tw-gap-2">
-          <NumberBullet>{{ i+1 }}</NumberBullet>
-          <div class="md:tw-text-xl tw-text-base tw-font-medium" v-html="step"></div>
+        <div
+          v-for="(step, i) in howItWorksSteps"
+          :key="i"
+          class="tw-flex tw-items-center tw-gap-2"
+        >
+          <NumberBullet>{{ i + 1 }}</NumberBullet>
+          <div
+            class="md:tw-text-xl tw-text-base tw-font-medium"
+            v-html="step"
+          ></div>
         </div>
       </div>
-      <div class="
-        tw-mt-10 tw-mb-6 md:tw-mt-20 md:tw-mb-12 
-        tw-text-3xl md:tw-text-6xl  
-        tw-font-medium tw-text-center
-      ">It's that simple.</div>
-      <v-img 
+      <div
+        class="tw-mt-10 tw-mb-6 md:tw-mt-20 md:tw-mb-12 tw-text-3xl md:tw-text-6xl tw-font-medium tw-text-center"
+      >
+        It's that simple.
+      </div>
+      <v-img
         alt="schej character"
         src="@/assets/schej_character.png"
         :max-height="isPhone ? 200 : 300"
@@ -95,13 +108,9 @@
     </div>
 
     <!-- Video -->
-    <div class="
-      tw-flex tw-bg-green 
-      tw-px-4 
-      tw-pt-20 
-      tw-pb-4 md:tw-pb-12
-      tw-justify-center
-    ">
+    <div
+      class="tw-flex tw-bg-green tw-px-4 tw-pt-20 tw-pb-4 md:tw-pb-12 tw-justify-center"
+    >
       <div class="md:tw-h-96 sm:tw-h-80 tw-h-64 tw-max-w-3xl tw-flex-1">
         <iframe
           class="tw-w-full tw-h-full"
@@ -116,21 +125,13 @@
 
     <!-- FAQ -->
     <div class="tw-flex tw-justify-center tw-pt-12">
-      <div
-        class="tw-flex-1 tw-mx-4 sm:tw-mx-16 tw-mb-12 tw-max-w-3xl"
-      >
+      <div class="tw-flex-1 tw-mx-4 sm:tw-mx-16 tw-mb-12 tw-max-w-3xl">
         <div id="faq-section" class="lg:tw-pt-3 tw-text-center">
-          <Header>
-            Frequently Asked Questions
-          </Header>
+          <Header> Frequently Asked Questions </Header>
           <div
             class="tw-grid tw-grid-cols-1 tw-gap-3 sm:tw-text-xl lg:tw-text-2xl"
           >
-            <FAQ 
-              v-for="faq in faqs"
-              :key="faq.question"
-              v-bind="faq"
-            />
+            <FAQ v-for="faq in faqs" :key="faq.question" v-bind="faq" />
           </div>
         </div>
       </div>
@@ -141,7 +142,7 @@
       <div
         class="tw-flex tw-flex-row tw-justify-around tw-m-2 tw-py-4 tw-font-medium"
       >
-        <a class="tw-text-white" href="#how-it-works" >How it works</a>
+        <a class="tw-text-white" href="#how-it-works">How it works</a>
         <router-link class="tw-text-white" :to="{ path: 'privacy-policy' }"
           >Privacy Policy</router-link
         >
@@ -152,12 +153,12 @@
 </template>
 
 <script>
-import LandingPageCalendar from "@/components/LandingPageCalendar.vue"
-import { isPhone, signInGoogle } from "@/utils"
-import SignInGoogleBtn from "@/components/SignInGoogleBtn.vue"
-import FAQ from "@/components/FAQ.vue"
-import Header from "@/components/Header.vue"
-import NumberBullet from "@/components/NumberBullet.vue"
+import LandingPageCalendar from "@/components/LandingPageCalendar.vue";
+import { isPhone, signInGoogle } from "@/utils";
+import SignInGoogleBtn from "@/components/SignInGoogleBtn.vue";
+import FAQ from "@/components/FAQ.vue";
+import Header from "@/components/Header.vue";
+import NumberBullet from "@/components/NumberBullet.vue";
 
 export default {
   name: "Landing",
@@ -167,8 +168,8 @@ export default {
     SignInGoogleBtn,
     FAQ,
     Header,
-    NumberBullet
-},
+    NumberBullet,
+  },
 
   data: () => ({
     dialog: false,
@@ -178,55 +179,63 @@ export default {
       SIGN_UP: 1,
     },
     howItWorksSteps: [
-      'Create a schej',
+      "Create a schej",
       '<span class="tw-underline tw-decoration-pale-green tw-decoration-4" style="text-underline-position: under;">Automatically</span> fill out your availability with Google Calendar',
-      'Share the schej and find the best time to meet!',
+      "Share the schej and find the best time to meet!",
     ],
     faqs: [
       {
-        question: 'Do I need to sign in with Google in order to use schej?',
-        answer: 'Signing in with Google is required to create events, but anybody can add their availability once an event is created, whether or not they are signed in with Google!',
+        question: "Do I need to sign in with Google in order to use schej?",
+        answer:
+          "Signing in with Google is required to create events, but anybody can add their availability once an event is created, whether or not they are signed in with Google!",
       },
       {
-        question: 'How is schej different from lettucemeet or when2meet?',
-        points: ['Much better UI (web and mobile)', 'Seemless and working calendar integration', 'No ads :)'],
+        question: "How is schej different from lettucemeet or when2meet?",
+        points: [
+          "Much better UI (web and mobile)",
+          "Seemless and working calendar integration",
+          "No ads :)",
+        ],
       },
       {
-        question: 'Is Google Calendar access required in order to use schej?',
-        answer: 'Nope! You can manually input your availability, but we highly recommend allowing Google Calendar access in order to view your calendar events while doing so.'
+        question: "Is Google Calendar access required in order to use schej?",
+        answer:
+          "Nope! You can manually input your availability, but we highly recommend allowing Google Calendar access in order to view your calendar events while doing so.",
       },
       {
-        question: 'Will other people be able to see my calendar events?',
-        answer: 'Nope! All other users will be able to see is the availability that you enter for an event.',
+        question: "Will other people be able to see my calendar events?",
+        answer:
+          "Nope! All other users will be able to see is the availability that you enter for an event.",
       },
       {
-        question: 'How do I edit my availability?',
-        answer: 'If you are signed in, simply click the "Edit availability" button. If you entered your availability as a guest, click on your name first and then "Edit availability".',
+        question: "How do I edit my availability?",
+        answer:
+          'If you are signed in, simply click the "Edit availability" button. If you entered your availability as a guest, click on your name first and then "Edit availability".',
       },
     ],
   }),
 
   computed: {
     isPhone() {
-      return isPhone(this.$vuetify)
+      return isPhone(this.$vuetify);
     },
     logoWidth() {
-      return 90
+      return 90;
     },
   },
 
   methods: {
     signInGoogle() {
-      signInGoogle(null, true)
+      signInGoogle(null, true);
     },
     signIn() {
-      this.dialog = true
-      this.dialogType = this.DIALOG_TYPES.SIGN_IN
+      this.dialog = true;
+      this.dialogType = this.DIALOG_TYPES.SIGN_IN;
     },
     getStarted() {
-      this.dialog = true
-      this.dialogType = this.DIALOG_TYPES.SIGN_UP
+      this.dialog = true;
+      this.dialogType = this.DIALOG_TYPES.SIGN_UP;
     },
   },
-}
+};
 </script>
