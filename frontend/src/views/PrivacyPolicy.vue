dd3e27faba871b814ea83fd08f383281fdf74728
Jonathan Liu <liu.z.jonathan@gmail.com>
2023-04-28 02:01:30 -0700

    Update PrivacyPolicy.vue

--- frontend/src/views/PrivacyPolicy.vue
+++ frontend/src/views/PrivacyPolicy.vue
@@ -1,3 +1,7 @@
 <template>
-  <iframe src="https://drive.google.com/file/d/1yRDL3NbqjAiRzGgo_0U5UVg3AAmO_35b/preview" style="width:100%; height:100%; position: absolute; left: 0;" frameborder="0"></iframe>
+  <iframe
+    src="https://docs.google.com/document/d/1WfcSi7FkNACWsm31sUiIcgpqIWWJ0mMm7qqma_x0kIM/preview"
+    style="width: 100%; height: 100%; position: absolute; left: 0"
+    frameborder="0"
+  ></iframe>
 </template>
