67e604d16e2da64c4cd1ed0c2ac9d3446906615b
Jonathan Liu <liu.z.jonathan@gmail.com>
2023-07-07 23:26:24 +0900

    increased authusermenu padding

--- frontend/src/components/AuthUserMenu.vue
+++ frontend/src/components/AuthUserMenu.vue
@@ -1,6 +1,6 @@
 <!-- Displays auth user's avatar, which displays a menu when clicked -->
 <template>
-  <v-menu v-if="authUser" offset-y left :close-on-content-click="false">
+  <v-menu v-if="authUser" offset-y left>
     <template v-slot:activator="{ on }">
       <v-btn id="user-menu-btn" icon :width="size" :height="size" v-on="on">
         <v-avatar :size="size">
@@ -10,21 +10,28 @@
     </template>
     <v-list class="py-0" :dense="isPhone">
       <v-list-item>
-        <v-list-item-title
-          ><strong>{{
-            `${authUser.firstName} ${authUser.lastName}`
-          }}</strong></v-list-item-title
-        >
+        <v-list-item-title>
+          <strong>{{ `${authUser.firstName} ${authUser.lastName}` }}</strong>
+        </v-list-item-title>
       </v-list-item>
       <v-list-item id="feedback-btn" @click="giveFeedback">
-        <v-list-item-title class="tw-flex tw-items-center tw-gap-1"><v-icon small color="black">mdi-message</v-icon>Give feedback</v-list-item-title>
+        <v-list-item-title class="tw-flex tw-items-center tw-gap-1">
+          <v-icon class="tw-mr-1" small color="black">mdi-message</v-icon>
+          Give feedback
+        </v-list-item-title>
       </v-list-item>
       <v-list-item id="settings-btn" @click="goToSettings">
-        <v-list-item-title class="tw-flex tw-items-center tw-gap-1"><v-icon small color="black">mdi-cog</v-icon>Settings</v-list-item-title>
+        <v-list-item-title class="tw-flex tw-items-center tw-gap-1">
+          <v-icon class="tw-mr-1" small color="black">mdi-cog</v-icon>
+          Settings
+        </v-list-item-title>
       </v-list-item>
       <v-divider></v-divider>
       <v-list-item id="sign-out-btn" @click="signOut">
-        <v-list-item-title class="red--text tw-flex tw-items-center tw-gap-1"><v-icon small color="red">mdi-logout</v-icon> Sign Out</v-list-item-title>
+        <v-list-item-title class="red--text tw-flex tw-items-center tw-gap-1">
+          <v-icon class="tw-mr-1" small color="red">mdi-logout</v-icon>
+          Sign Out
+        </v-list-item-title>
       </v-list-item>
     </v-list>
   </v-menu>
@@ -53,18 +60,18 @@ export default {
   },
 
   methods: {
-      ...mapMutations([ 'setAuthUser' ]),
-      giveFeedback() {
-        window.open('https://forms.gle/9AgRy4PQfWfVuBnw8', '_blank');
-      },
-      async signOut() {
-        await post('/auth/sign-out')
-        this.setAuthUser(null)
-        location.reload()
-      },
-      goToSettings() {
-        this.$router.replace({ name: "settings" })
-      },
+    ...mapMutations(["setAuthUser"]),
+    giveFeedback() {
+      window.open("https://forms.gle/9AgRy4PQfWfVuBnw8", "_blank")
+    },
+    async signOut() {
+      await post("/auth/sign-out")
+      this.setAuthUser(null)
+      location.reload()
+    },
+    goToSettings() {
+      this.$router.replace({ name: "settings" })
+    },
   },
 }
 </script>
