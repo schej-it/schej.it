c5164177abcf9052f744991feb616ed00f3f63e1
Jonathan Liu <liu.z.jonathan@gmail.com>
2024-05-07 15:19:45 -0700

    mobile ad

--- frontend/src/components/event/Advertisement.vue
+++ frontend/src/components/event/Advertisement.vue
@@ -1,12 +1,22 @@
 <template>
-  <div v-if="showAd" class="tw-relative tw-cursor-pointer" @click="navigateToAd">
+  <div
+    v-if="showAd"
+    class="tw-relative tw-cursor-pointer"
+    @click="navigateToAd"
+  >
     <v-img
       alt="tomotime ad"
-      src="@/assets/ads/tomotime.png"
+      :src="
+        isPhone
+          ? require('@/assets/ads/tomotime_mobile.png')
+          : require('@/assets/ads/tomotime.png')
+      "
       transition="fade-transition"
       class="tw-max-w-[60rem]"
     />
-    <div class="tw-bg-gray tw-bg-opacity-50 tw-w-8 tw-h-8 sm:tw-w-10 sm:tw-h-10 tw-absolute tw-left-0 tw-top-0 tw-flex tw-justify-center tw-items-center tw-rounded-tl-lg tw-rounded-br-lg tw-ml-0 tw-mt-0 sm:tw-mt-2 sm:tw-ml-2 tw-text-xs ">
+    <div
+      class="tw-absolute tw-left-0 tw-top-0 tw-ml-0 tw-mt-0 tw-flex tw-h-8 tw-w-8 tw-items-center tw-justify-center tw-rounded-br-lg tw-rounded-tl-lg tw-bg-gray tw-bg-opacity-50 tw-text-xs sm:tw-ml-2 sm:tw-mt-2 sm:tw-h-10 sm:tw-w-10"
+    >
       AD
     </div>
   </div>
@@ -14,20 +24,23 @@
 
 <script>
 import { mapActions, mapState } from "vuex"
-import {
-  isPhone,
-} from "@/utils"
+import { isPhone } from "@/utils"
 
 export default {
   name: "Advertisement",
 
-  props: {
-  },
+  props: {},
 
   data: () => ({
     link: "https://tomotime.app",
     eduOnly: true,
   }),
+
+  computed: {
+    isPhone() {
+      return isPhone(this.$vuetify)
+    },
+  },
 
   methods: {
     navigateToAd() {
@@ -35,12 +48,10 @@ export default {
         link: this.link,
       })
       window.open(this.link, "_blank")
-    }
+    },
   },
 
-  watch: {
-    
-  },
+  watch: {},
 
   computed: {
     ...mapState(["authUser"]),
@@ -58,6 +69,6 @@ export default {
     },
   },
 
-  components: {  },
+  components: {},
 }
 </script>
