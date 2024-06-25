312bba15a3e8afd1341800042be879f98b961742
Jonathan Liu <liu.z.jonathan@gmail.com>
2024-06-25 11:29:40 -0500

    fix tailwind?

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,7 +1,11 @@
 const colors = require("tailwindcss/colors")
 
 module.exports = {
-  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  content: [
+    "./index.html",
+    "./public/**/*.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
   important: true,
   theme: {
     extend: {
