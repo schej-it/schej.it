d5d1a90d7025ed6c50bb54cb54eefc0bfbeee9d5
Jonathan Liu <liu.z.jonathan@gmail.com>
2024-10-13 18:42:21 +0000

    reboot server script fixes

--- deploy_scripts/reboot_server_if_down.sh
+++ deploy_scripts/reboot_server_if_down.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
+# Define the output file
+output_file="/schej.it/deploy_scripts/reboot_log.txt"
+
 # Run screen -ls and capture the output
 screen_output=$(screen -ls)
 
+# Get current timestamp
+timestamp=$(date "+%Y-%m-%d %H:%M:%S")
+
 # Check if the output contains "schej.it-server"
+cd /schej.it/deploy_scripts
 if echo "$screen_output" | grep -q "schej.it-server"; then
-    echo "Screen 'schej.it-server' is running."
+    echo "[$timestamp] Screen 'schej.it-server' is running." >> "$output_file"
 else
-    echo "Screen 'schej.it-server' is not running! Rebooting..."
+    echo "[$timestamp] Screen 'schej.it-server' is not running! Rebooting..." >> "$output_file"
     cd ../server
     screen -dmS schej.it-server sudo ./server -release=true
 fi
