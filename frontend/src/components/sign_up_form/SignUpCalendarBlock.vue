b1a983e5a587d84297449f54863556036892774c
Tony Xin <32372239+TtonyxX@users.noreply.github.com>
2024-11-24 13:07:46 -0800

    [FIX] Rounded borders for calendar blocks

--- frontend/src/components/sign_up_form/SignUpCalendarBlock.vue
+++ frontend/src/components/sign_up_form/SignUpCalendarBlock.vue
@@ -1,24 +1,27 @@
 <template>
-  <div class="tw-bg-white tw-h-full tw-w-full tw-cursor-pointer">
   <div
-    class="tw-h-full tw-w-full tw-overflow-hidden tw-text-ellipsis tw-border-2 tw-border-solid tw-p-1 tw-text-xs"
+    class="tw-h-full tw-w-full tw-cursor-pointer tw-overflow-hidden tw-rounded-md tw-border-2 tw-border-solid tw-bg-white"
     :class="unsaved ? 'tw-border-light-green' : 'tw-border-gray'"
-    :style="{ backgroundColor: backgroundColor }"
   >
-  <div v-if="!titleOnly">
-    <div class="ph-no-capture tw-font-medium" :class="fontColor">
-      {{ signUpBlock.name }}
-    </div>
-    <div class="ph-no-capture tw-font-medium" :class="fontColor">
-      ({{ numberResponses }}/{{ signUpBlock.capacity }})
+    <div
+      class="tw-h-full tw-w-full tw-overflow-hidden tw-text-ellipsis tw-p-1 tw-text-xs"
+      :style="{ backgroundColor: backgroundColor }"
+    >
+      <div v-if="!titleOnly">
+        <div class="ph-no-capture tw-font-medium" :class="fontColor">
+          {{ signUpBlock.name }}
+        </div>
+        <div class="ph-no-capture tw-font-medium" :class="fontColor">
+          ({{ numberResponses }}/{{ signUpBlock.capacity }})
+        </div>
+      </div>
+      <div v-else>
+        <div class="tw-text-xs tw-italic" :class="fontColor">
+          {{ title }}
+        </div>
+      </div>
     </div>
   </div>
-  <div v-else>
-    <div class="tw-text-xs tw-italic" :class="fontColor">
-      {{ title }}
-    </div>
-  </div>
-  </div></div>
 </template>
 
 <script>
@@ -32,36 +35,35 @@ export default {
     title: { type: String, default: "" },
   },
 
-  data: () => ({
-
-  }),
+  data: () => ({}),
 
   computed: {
     numberResponses() {
-      return this.signUpBlock && this.signUpBlock.responses ? this.signUpBlock.responses.length : 0;
+      return this.signUpBlock && this.signUpBlock.responses
+        ? this.signUpBlock.responses.length
+        : 0
     },
     backgroundColor() {
       const capacity = this.signUpBlock ? this.signUpBlock.capacity : 1
       const frac = this.numberResponses / capacity
       const green = "#00994C"
       let alpha = Math.floor(frac * (255 - 30))
-          .toString(16)
-          .toUpperCase()
-          .substring(0, 2)
-          .padStart(2, "0")
-      if (
-        frac == 1
-      ) {
+        .toString(16)
+        .toUpperCase()
+        .substring(0, 2)
+        .padStart(2, "0")
+      if (frac == 1) {
         alpha = "FF"
       }
       return `${green}${alpha}`
     },
     fontColor() {
-      return this.numberResponses == this.signUpBlock?.capacity && !this.unsaved ? "tw-text-white" : "tw-text-dark-gray"
-    }
+      return this.numberResponses == this.signUpBlock?.capacity && !this.unsaved
+        ? "tw-text-white"
+        : "tw-text-dark-gray"
+    },
   },
 
-  methods: {
-  },
+  methods: {},
 }
 </script>
