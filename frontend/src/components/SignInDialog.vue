4f7388b5e2f92119bae6c1761e476ef32ce9e460
Jonathan Liu <liu.z.jonathan@gmail.com>
2025-05-01 12:41:08 -0700

    added sign in with outlook button

--- frontend/src/components/SignInDialog.vue
+++ frontend/src/components/SignInDialog.vue
@@ -8,11 +8,42 @@
     <v-card>
       <v-card-title>Sign in</v-card-title>
       <v-card-text class="tw-flex tw-flex-col tw-items-center">
-        <!-- TODO: Add Sign in with Outlook -->
-        <SignInGoogleBtn
-          class="tw-mb-4"
-          @click="signIn(calendarTypes.GOOGLE)"
-        />
+        <div class="tw-mb-4 tw-flex tw-w-full tw-flex-col tw-gap-2">
+          <v-btn
+            block
+            @click="signIn(calendarTypes.GOOGLE)"
+            class="tw-bg-white"
+          >
+            <div class="tw-flex tw-w-full tw-items-center tw-gap-2">
+              <v-img
+                class="tw-flex-initial"
+                width="20"
+                height="20"
+                src="@/assets/google_logo.svg"
+              />
+              <v-spacer />
+              Sign in with Google
+              <v-spacer />
+            </div>
+          </v-btn>
+          <v-btn
+            block
+            @click="signIn(calendarTypes.OUTLOOK)"
+            class="tw-bg-white"
+          >
+            <div class="tw-flex tw-w-full tw-items-center tw-gap-2">
+              <v-img
+                class="tw-flex-initial"
+                width="20"
+                height="20"
+                src="@/assets/outlook_logo.svg"
+              />
+              <v-spacer />
+              Sign in with Outlook
+              <v-spacer />
+            </div>
+          </v-btn>
+        </div>
         <div class="tw-text-center tw-text-xs">
           By continuing, you agree to our
           <router-link class="tw-text-blue" :to="{ name: 'privacy-policy' }"
