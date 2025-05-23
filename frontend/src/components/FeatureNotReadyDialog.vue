5cdee65d10021e56f2cab115f13c8e0c42d3e9ef
Jonathan Liu <liu.z.jonathan@gmail.com>
2025-05-23 13:57:27 -0700

    added text box for folder dialog

--- frontend/src/components/FeatureNotReadyDialog.vue
+++ frontend/src/components/FeatureNotReadyDialog.vue
@@ -13,23 +13,39 @@
           <v-icon>mdi-close</v-icon>
         </v-btn>
       </v-card-title>
-      <v-card-text class="tw-pt-4 tw-text-very-dark-gray">
-        You've caught us a bit early! This feature isn't quite ready yet. We're
-        working on it and will let you know when "Folders" are released.
+      <v-card-text class="tw-text-very-dark-gray">
+        You've caught us a bit early! We're considering adding folders to Schej,
+        and will do so once we get enough demand from users.
+        <v-textarea
+          v-model="folderUsageFeedback"
+          label="What would you like to use folders for?"
+          rows="3"
+          class="tw-mt-4"
+          outlined
+          dense
+        ></v-textarea>
       </v-card-text>
       <v-card-actions>
         <v-spacer></v-spacer>
-        <v-btn color="primary" text @click="dialog = false">Got it!</v-btn>
+        <v-btn text @click="dialog = false">Close</v-btn>
+        <v-btn color="primary" @click="submitFeedback">Submit</v-btn>
       </v-card-actions>
     </v-card>
   </v-dialog>
 </template>
 
 <script>
+import { mapActions } from "vuex"
+
 export default {
   name: "FeatureNotReadyDialog",
   props: {
     value: Boolean,
+  },
+  data() {
+    return {
+      folderUsageFeedback: "",
+    }
   },
   computed: {
     dialog: {
@@ -39,6 +55,23 @@ export default {
       set(val) {
         this.$emit("input", val)
       },
+    },
+  },
+  methods: {
+    ...mapActions(["showInfo"]),
+    submitFeedback() {
+      if (this.folderUsageFeedback.trim() !== "") {
+        this.$posthog?.capture("folder_usage_feedback_submitted", {
+          feedback: this.folderUsageFeedback,
+        })
+        // Optionally, you can clear the textarea and close the dialog
+        this.folderUsageFeedback = ""
+        this.dialog = false
+        this.showInfo("Thanks for your input!")
+      } else {
+        // Optionally, handle empty feedback (e.g., show a message)
+        console.log("Feedback is empty")
+      }
     },
   },
 }
