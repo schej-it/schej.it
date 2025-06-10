bac20c57c6567f06493f7de8df4b5c74013feb5f
Jonathan Liu <liu.z.jonathan@gmail.com>
2025-06-09 18:19:28 -0700

    add team member button

--- frontend/src/components/AuthUserMenu.vue
+++ frontend/src/components/AuthUserMenu.vue
@@ -1,57 +1,76 @@
 <!-- Displays auth user's avatar, which displays a menu when clicked -->
 <template>
-  <v-menu v-if="authUser" offset-y left>
-    <template v-slot:activator="{ on }">
-      <v-btn id="user-menu-btn" icon :width="size" :height="size" v-on="on">
-        <v-avatar :size="size">
-          <UserAvatarContent :user="authUser" :size="size" />
-        </v-avatar>
-      </v-btn>
-    </template>
-    <v-list class="py-0" :dense="isPhone">
-      <v-list-item>
-        <v-list-item-title>
-          <strong>{{ `${authUser.firstName} ${authUser.lastName}` }}</strong>
-        </v-list-item-title>
-      </v-list-item>
-      <v-list-item
-        v-if="showFeedbackBtn"
-        id="feedback-btn"
-        href="https://forms.gle/9AgRy4PQfWfVuBnw8"
-        target="_blank"
-      >
-        <v-list-item-title class="tw-flex tw-items-center tw-gap-1">
-          <v-icon class="tw-mr-1" small color="black">mdi-message</v-icon>
-          Give feedback
-        </v-list-item-title>
-      </v-list-item>
-      <v-list-item id="settings-btn" @click="goToSettings">
-        <v-list-item-title class="tw-flex tw-items-center tw-gap-1">
-          <v-icon class="tw-mr-1" small color="black">mdi-cog</v-icon>
-          Settings
-        </v-list-item-title>
-      </v-list-item>
-      <v-divider></v-divider>
-      <v-list-item id="sign-out-btn" @click="signOut">
-        <v-list-item-title class="red--text tw-flex tw-items-center tw-gap-1">
-          <v-icon class="tw-mr-1" small color="red">mdi-logout</v-icon>
-          Sign Out
-        </v-list-item-title>
-      </v-list-item>
-    </v-list>
-  </v-menu>
+  <span>
+    <v-menu v-if="authUser" offset-y left>
+      <template v-slot:activator="{ on }">
+        <v-btn id="user-menu-btn" icon :width="size" :height="size" v-on="on">
+          <v-avatar :size="size">
+            <UserAvatarContent :user="authUser" :size="size" />
+          </v-avatar>
+        </v-btn>
+      </template>
+      <v-list class="py-0" :dense="isPhone">
+        <v-list-item>
+          <v-list-item-title>
+            <strong>{{ `${authUser.firstName} ${authUser.lastName}` }}</strong>
+          </v-list-item-title>
+        </v-list-item>
+        <v-list-item id="add-team-member-btn" @click="addTeamMember">
+          <v-list-item-title class="tw-flex tw-items-center tw-gap-1">
+            <v-icon class="tw-mr-1" small color="black"
+              >mdi-account-plus</v-icon
+            >
+            Add team member
+          </v-list-item-title>
+        </v-list-item>
+        <v-list-item
+          v-if="showFeedbackBtn"
+          id="feedback-btn"
+          href="https://forms.gle/9AgRy4PQfWfVuBnw8"
+          target="_blank"
+        >
+          <v-list-item-title class="tw-flex tw-items-center tw-gap-1">
+            <v-icon class="tw-mr-1" small color="black">mdi-message</v-icon>
+            Give feedback
+          </v-list-item-title>
+        </v-list-item>
+        <v-list-item id="settings-btn" @click="goToSettings">
+          <v-list-item-title class="tw-flex tw-items-center tw-gap-1">
+            <v-icon class="tw-mr-1" small color="black">mdi-cog</v-icon>
+            Settings
+          </v-list-item-title>
+        </v-list-item>
+        <v-divider></v-divider>
+        <v-list-item id="sign-out-btn" @click="signOut">
+          <v-list-item-title class="red--text tw-flex tw-items-center tw-gap-1">
+            <v-icon class="tw-mr-1" small color="red">mdi-logout</v-icon>
+            Sign Out
+          </v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-menu>
+    <TeamsNotReadyDialog v-model="showTeamsNotReadyDialog" />
+  </span>
 </template>
 
 <script>
 import UserAvatarContent from "@/components/UserAvatarContent"
 import { mapState, mapMutations } from "vuex"
 import { post, isPhone } from "@/utils"
+import TeamsNotReadyDialog from "./TeamsNotReadyDialog.vue"
 
 export default {
   name: "AuthUserMenu",
 
   components: {
     UserAvatarContent,
+    TeamsNotReadyDialog,
+  },
+
+  data() {
+    return {
+      showTeamsNotReadyDialog: false,
+    }
   },
 
   computed: {
@@ -77,6 +96,10 @@ export default {
     },
     goToSettings() {
       this.$router.replace({ name: "settings" })
+    },
+    addTeamMember() {
+      this.$posthog?.capture("add_team_member_clicked")
+      this.showTeamsNotReadyDialog = true
     },
   },
 }
---
+++ frontend/src/components/TeamsNotReadyDialog.vue
@@ -0,0 +1,56 @@
+<template>
+  <v-dialog v-model="dialog" max-width="400px" content-class="tw-m-0">
+    <v-card>
+      <v-card-title>
+        <span class="tw-text-xl tw-font-medium">Upgrade Required</span>
+        <v-spacer />
+        <v-btn
+          absolute
+          @click="dialog = false"
+          icon
+          class="tw-right-0 tw-mr-2 tw-self-center"
+        >
+          <v-icon>mdi-close</v-icon>
+        </v-btn>
+      </v-card-title>
+      <v-card-text class="tw-text-very-dark-gray">
+        Teams are only available on the Schej organization plan. Book a call
+        with the founder to learn about how to upgrade.
+      </v-card-text>
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn text @click="dialog = false">Close</v-btn>
+        <v-btn color="primary" @click="bookCall">Book a call</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+export default {
+  name: "TeamsNotReadyDialog",
+  props: {
+    value: Boolean,
+  },
+  computed: {
+    dialog: {
+      get() {
+        return this.value
+      },
+      set(val) {
+        this.$emit("input", val)
+      },
+    },
+  },
+  methods: {
+    bookCall() {
+      this.$posthog?.capture("book_call_for_organization_plan_clicked")
+      window.open(
+        "https://cal.com/jonathan-liu/schej-organization-plan",
+        "_blank"
+      )
+      this.dialog = false
+    },
+  },
+}
+</script>
