802c0519c37c693eb6c4779f0e1c7e56d5ec2aa1
Jonathan Liu <liu.z.jonathan@gmail.com>
2025-06-13 14:31:20 -0700

    started dashboard section

---
+++ frontend/src/components/home/Dashboard.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="tw-rounded-md tw-px-6 tw-py-4 sm:tw-mx-4 sm:tw-bg-[#f3f3f366]">
+    <div class="tw-mb-3 tw-flex tw-items-center tw-justify-between">
+      <div class="tw-text-xl tw-font-medium tw-text-dark-green sm:tw-text-2xl">
+        Dashboard
+      </div>
+      <v-btn
+        text
+        @click="createFolder"
+        class="tw-hidden tw-text-very-dark-gray sm:tw-block"
+      >
+        <v-icon class="tw-mr-2 tw-text-lg">mdi-folder-plus</v-icon>
+        New folder
+      </v-btn>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Dashboard",
+
+  methods: {
+    createFolder() {},
+  },
+}
+</script>
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -24,32 +24,9 @@
         :width="2"
       ></v-progress-circular>
     </div>
-    <template v-if="groupsEnabled">
-      <v-fade-transition>
-        <div
-          class="tw-rounded-md tw-px-6 tw-py-4 sm:tw-mx-4 sm:tw-bg-[#f3f3f366]"
-          v-if="!loading || eventsNotEmpty"
-        >
-          <EventType
-            :eventType="availabilityGroups"
-            emptyText="You are not part of any availability groups!"
-          />
-        </div>
-      </v-fade-transition>
-    </template>
+
     <v-fade-transition>
-      <div
-        class="tw-rounded-md tw-px-6 tw-py-4 sm:tw-mx-4 sm:tw-bg-[#f3f3f366]"
-        v-if="!loading || eventsNotEmpty"
-      >
-        <div class="tw-grid tw-gap-4 sm:tw-gap-8">
-          <EventType
-            v-for="(eventType, t) in events"
-            :key="t"
-            :eventType="eventType"
-          ></EventType>
-        </div>
-      </div>
+      <Dashboard v-if="!loading || eventsNotEmpty" />
     </v-fade-transition>
 
     <div
@@ -101,6 +78,7 @@ import EventType from "@/components/EventType.vue"
 import BottomFab from "@/components/BottomFab.vue"
 import CreateSpeedDial from "@/components/CreateSpeedDial.vue"
 import When2meetImportDialog from "@/components/When2meetImportDialog.vue"
+import Dashboard from "@/components/home/Dashboard.vue"
 import { mapState, mapActions, mapMutations } from "vuex"
 import { eventTypes } from "@/constants"
 import { isPhone, get } from "@/utils"
@@ -117,6 +95,7 @@ export default {
     BottomFab,
     CreateSpeedDial,
     When2meetImportDialog,
+    Dashboard,
   },
 
   props: {
@@ -191,6 +170,7 @@ export default {
         openNewGroup: false,
       })
     },
+    createFolder() {},
     convertW2M() {
       this.showW2MDialog = true
       this.$posthog?.capture("convert_when2meet_to_schej_clicked")
