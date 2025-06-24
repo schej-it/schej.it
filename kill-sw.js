e8deeee4f658f97dd5ca1cedbc8e930b84a51823
Jonathan Liu <liu.z.jonathan@gmail.com>
2025-06-24 15:16:38 -0700

    Create kill-sw.js

---
+++ kill-sw.js
@@ -0,0 +1,15 @@
+// /schej.it/kill-sw.js
+self.addEventListener("install", function (e) {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", function (e) {
+  self.registration
+    .unregister()
+    .then(function () {
+      return self.clients.matchAll();
+    })
+    .then(function (clients) {
+      clients.forEach((client) => client.navigate(client.url));
+    });
+});
